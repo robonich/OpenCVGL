042443b485227927ac18a920d89f25170a27b51f
Nisho Matsushita <robonich@gmail.com>
2015-10-01 09:57:10 +0900

    complete problem2_1

--- OpenGLKadai/problem2_1.cpp
+++ OpenGLKadai/problem2_1.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 #define WINDOW_X (500)
@@ -13,12 +14,15 @@ void set_callback_functions();
 
 void glut_display();
 void glut_keyboard(unsigned char key, int x, int y);
+void specialkeydown(int key, int x, int y);
 
 void draw_square1();
 void draw_square2();
 void draw_square3();
 void draw_hexagon();
 void draw_nxagon(int n);
+
+
 
 // グローバル変数
 int g_display_mode = 1;
@@ -36,7 +40,8 @@ int main(int argc, char *argv[]){
 	set_callback_functions();
 
 	/* メインループ */
-	glutMainLoop();                          // 無限ループ。コールバック関数が呼ばれるまでずっと実行される。
+	glutMainLoop();  
+    // 無限ループ。コールバック関数が呼ばれるまでずっと実行される。
 
 	return 0;
 }
@@ -55,6 +60,30 @@ void init(){
 void set_callback_functions(){
 	glutDisplayFunc(glut_display);            // ディスプレイに変化があった時に呼ばれるコールバック関数を登録
 	glutKeyboardFunc(glut_keyboard);          // キーボードに変化があった時に呼び出されるコールバック関数を登録
+	glutSpecialFunc(specialkeydown);          //矢印キーなどの特殊入力を検知する
+}
+
+void specialkeydown(int key, int x, int y)
+{
+     if( key == GLUT_KEY_UP )//矢印「上」
+     {
+		nxagon++;
+     }
+
+     if( key == GLUT_KEY_DOWN )//矢印「下」
+     {
+		if(nxagon > 3)
+			nxagon--;
+     }
+
+     if( key == GLUT_KEY_LEFT )
+     {
+     }
+
+     if( key == GLUT_KEY_RIGHT )
+     {
+     }
+     glutPostRedisplay();
 }
 
 // キーボードに変化があった時に呼び出されるコールバック関数。
@@ -76,13 +105,10 @@ void glut_keyboard(unsigned char key, int x, int y){
 		break;
 	case '4':
 	    g_display_mode = 4;
+	    break;
 	case 'n':
 		g_display_mode = 5;
-	case 38:
-		nxagon++;
-	case 40:
-		if(nxagon > 3)
-			nxagon--;
+		break;
 	}
 
 	glutPostRedisplay(); // 「ディスプレイのコールバック関数を呼んで」と指示する。
@@ -105,8 +131,10 @@ void glut_display(){
 		break;
 	case 4:
 		draw_hexagon();
+		break;
 	case 5:
 		draw_nxagon(nxagon);
+		break;
 	}
 
 	glFlush(); // ここで画面に描画をする
@@ -171,21 +199,21 @@ void draw_nxagon(int n){
 
 	glColor3d(0.0, 0.0, 1.0);
 
-	double theta = 360.0 / (double)n;
+	double theta = 2 * M_PI / (double)n;
 
 	// double rotation[2][2] = {
 	// 	{cos(theta), -sin(theta)},
 	// 	{sin(theta), cos(theta)}
 	// }
 
-	double prevPoint[2], point[2] = {0.9, 0};
+	double prevPoint[2], point[2] = {0, 0.9};
 
 	for(int i = 0; i < n; i++){
-		glVertex2d(point[0], point[1]);
+		glVertex2dv(point);
 		prevPoint[0] = point[0];
 		prevPoint[1] = point[1];
 		point[0] = prevPoint[0] * cos(theta) - prevPoint[1] * sin(theta);
-		point[0] = prevPoint[0] * sin(theta) + prevPoint[1] * cos(theta);
+		point[1] = prevPoint[0] * sin(theta) + prevPoint[1] * cos(theta);
 	}
 	glEnd();
 }
