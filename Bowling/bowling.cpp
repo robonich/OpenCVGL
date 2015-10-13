8001d948aa842814283693e372a10b6958fe0a9f
Nisho Matsushita <robonich@gmail.com>
2015-10-14 00:58:37 +0900

    add force

--- Bowling/bowling.cpp
+++ Bowling/bowling.cpp
@@ -18,18 +18,20 @@ void set_callback_functions();
 void glut_display();
 void glut_keyboard(unsigned char key, int x, int y);
 void specialkeydown(int key, int x, int y);
+//void glut_timer(int value);
 
 class Sphere//sphere class
 {
 public:
 	double radius;
+  double mass;
 	//double pos[3];
 	std::vector<double> pos;//pos size is 3
 
 	enum DRAW_TYPE { SOLID = 0, WIRE = 1};
 
 	Sphere();
-	Sphere(double _radius, std::vector<double> _pos);
+  Sphere(double _radius, double _mass, std::vector<double> _pos);
 
 	void addForce(double x, double y, double z, double force);//ある方向に力を加える
 	void addForce(std::vector<double> axis, double force);//axisはsize 3
@@ -52,8 +54,13 @@ public:
 	void draw();
 };
 
-Sphere bowl(0.5, {0,0,0});
-Floor floor1({2,2}, {0,0,0}, 0);
+Sphere bowl(1.0, 0.5, {0,0.5,0});
+Floor floor1({2,10}, {0,0,0}, 0);
+
+bool upkeydown = false; //false: key up, true: key down
+bool downkeydown = false;
+bool leftkeydown = false;
+bool rightkeydown = false;
 
 int main(int argc, char *argv[]){
 	init_GL(argc, argv);
@@ -81,6 +88,8 @@ void init(){
 void set_callback_functions(){
 	glutDisplayFunc(glut_display);
 	glutKeyboardFunc(glut_keyboard);
+	glutSpecialFunc(specialkeydown);
+	//glutTimerFunc(1.0/DT, glut_timer, 0);
 }
 
 void glut_keyboard(unsigned char key, int x, int y){
@@ -97,21 +106,38 @@ void specialkeydown(int key, int x, int y)
 {
      if( key == GLUT_KEY_UP )//矢印「上」
      {
+       bowl.addForce({0.0, 0.0, -1.0}, 2.0);
+       upkeydown = true;
      }
 
      if( key == GLUT_KEY_DOWN )//矢印「下」
      {
+       bowl.addForce({0.0, 0.0, 1.0}, 2.0);
+       downkeydown = true;
      }
 
      if( key == GLUT_KEY_LEFT )
      {
+       bowl.addForce({-1.0, 0.0, 0.0}, 2.0);
+       leftkeydown = true;
      }
 
      if( key == GLUT_KEY_RIGHT )
      {
+       bowl.addForce({1.0, 0.0, 0.0}, 2.0);
+       rightkeydown = true;
      }
      glutPostRedisplay();
 }
+
+
+
+/*void glut_timer(int value){
+  if(upkeydown) bowl.addForce({0.0, 0.0, -1.0}, 2.0);
+  if(downkeydown) bowl.addForce({0.0, 0.0, 1.0}, 2.0);
+  if(leftkeydown) bowl.addForce({-1.0, 0.0, 0.0}, 2.0);
+  if(rightkeydown) bowl.addForce({1.0, 0.0, 0.0}, 2.0);
+}*/
 
 void glut_display(){
 	glMatrixMode(GL_PROJECTION);
@@ -120,7 +146,7 @@ void glut_display(){
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(20.0, 0.0, 0.0,
+	gluLookAt(0.0, 10.0, 20.0,
 		0.0, 0.0, 0.0,
 		0.0, 1.0, 0.0);
 	
@@ -128,7 +154,7 @@ void glut_display(){
 	glEnable(GL_DEPTH_TEST);
 
 	bowl.draw(Sphere::WIRE);
-	//floor1.draw();
+	floor1.draw();
 	//glutWireSphere(0.5, 50, 50);
 
 	glFlush();
@@ -137,27 +163,27 @@ void glut_display(){
 	glutSwapBuffers();
 }
 
-Sphere::Sphere() : Sphere(0, {0,0,0}){}
-Sphere::Sphere(double _radius, std::vector<double> _pos) : radius(_radius), pos(_pos){}
+Sphere::Sphere() : Sphere(0.25, 0.1, {0,0,0}){}
+Sphere::Sphere(double _radius, double _mass, std::vector<double> _pos) : radius(_radius), mass(_mass), pos(_pos){}
 
 void Sphere::addForce(double x, double y, double z, double force){
 	double mag = sqrt(x*x+y*y+z*z);
-	pos[0] += 0.5 * force * DT * DT * x / mag;
-	pos[1] += 0.5 * force * DT * DT * y / mag;
-	pos[2] += 0.5 * force * DT * DT * z / mag;
-
+	pos[0] += 0.5 * force / mass * DT * DT * x / mag;
+	pos[1] += 0.5 * force / mass * DT * DT * y / mag;
+	pos[2] += 0.5 * force / mass * DT * DT * z / mag;
 }//ある方向に力を加える
 void Sphere::addForce(std::vector<double> axis, double force){
 	addForce(axis[0], axis[1], axis[2], force);
 }
 void Sphere::draw(/*Sphere::*/DRAW_TYPE d){
 	glPushMatrix();
+	glColor3d(1.0, 1.0, 1.0);
 	glTranslatef(pos[0], pos[1], pos[2]);
 	switch(d){
 		case SOLID:
-		glutSolidSphere(radius, 50, 50);
+		glutSolidSphere(radius, 40, 40);
 		case WIRE:
-		glutWireSphere(radius, 50, 50);
+		glutWireSphere(radius, 40, 40);
 	}
 	glPopMatrix();
 }
@@ -176,11 +202,11 @@ bool Floor::isTouchedSphere(Sphere s){
 }
 
 void Floor::draw(){
-	glColor3d(1.0, 1.0, 1.0);
+	glColor3d(1.0, 1.0, 0.0);
 	glBegin(GL_POLYGON);
-	glVertex3d(pos[0]+0.5*size[0], pos[1]+0.5*size[1], pos[2]);
-	glVertex3d(pos[0]+0.5*size[0], pos[1]-0.5*size[1], pos[2]);
-	glVertex3d(pos[0]-0.5*size[0], pos[1]-0.5*size[1], pos[2]);
-	glVertex3d(pos[0]-0.5*size[0], pos[1]+0.5*size[1], pos[2]);
+	glVertex3d(pos[0]+0.5*size[0], pos[1], pos[2]+0.5*size[1]);
+	glVertex3d(pos[0]+0.5*size[0], pos[1], pos[2]-0.5*size[1]);
+	glVertex3d(pos[0]-0.5*size[0], pos[1], pos[2]-0.5*size[1]);
+	glVertex3d(pos[0]-0.5*size[0], pos[1], pos[2]+0.5*size[1]);
 	glEnd();
 }
